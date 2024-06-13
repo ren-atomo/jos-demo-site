b9ce760fc9e171d2288a6b7151d6f0c2ebd7581a
ren-atomo <140385083+ren-atomo@users.noreply.github.com>
2024-06-13 14:16:54 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,3 +3,10 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     alert('お問い合わせありがとうございます！');
     this.reset();
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const items = document.querySelectorAll(".flowchart-item");
+    items.forEach((item, index) => {
+        item.style.animationDelay = `${index * 0.5}s`;
+    });
+});
